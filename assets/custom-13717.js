ce3845d039c846eab345965179a8b44b121e12a3
Eduardo Franco <edo@boozt.la>
2026-07-21 17:29:02 -0500

    Initial commit: Shopify theme make-waves

---
+++ assets/custom-13717.js
@@ -0,0 +1,7 @@
+// Select the <h4> element
+const h4Element = document.querySelector(".two-column-container h4");
+
+// Update the inner text
+if (h4Element) {
+  h4Element.innerText = "We Strive To Be";
+}
